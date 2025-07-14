85334d6f075c4c3893ab02d255713e56438572a5
tv51818 <55032762+tv51818@users.noreply.github.com>
2025-07-14 20:21:55 +0800

    Update index-page.js

--- js/index-page.js
+++ js/index-page.js
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 try {
                     window.history.replaceState(
                         { search: keyword }, 
-                        `搜索: ${keyword} - LibreTV`, 
+                        `搜索: ${keyword} - 影搜`, 
                         window.location.href
                     );
                 } catch (e) {
